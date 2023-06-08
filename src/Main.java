2dfbec973c874d14f35a67a30107dcffbd6897ae
sachin jadhao <jadhaosachin7171@gmail.com>
2023-06-08 20:18:57 +0530

    k is always increase by two

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int k = 0;
+        int n = 6;
+
+        for(int i=0; i<n; i++){
+            k = k+2;
+        }
+
+        System.out.println(k);
+
+    }
+}
